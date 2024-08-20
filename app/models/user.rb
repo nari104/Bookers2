839252154eedc09443452f909ab85e80d0a057a5
nari104 <172471497+nari104@users.noreply.github.com>
2024-08-20 19:30:29 +0000

    [update] bookers2 20240821-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   has_one_attached :book
+  has_many :books, dependent: :destroy
 
   def get_book(width, height)
     unless book.attached?
