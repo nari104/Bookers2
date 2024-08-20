e177853c1f9364475533355f2796876427dcf454
nari104 <172471497+nari104@users.noreply.github.com>
2024-08-20 16:06:23 +0000

    [update] bookers2 20240821

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books, dependent: :destroy
+
+  has_one_attached :book
+
+  def get_book(width, height)
+    unless book.attached?
+      file_path = Rails.root.join('app/assets/images/sample-authour1.jpg')
+      book.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    book.variant(resize_to_limit: [width, height]).processed
+  end
 end
