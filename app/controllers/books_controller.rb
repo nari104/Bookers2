3f3b74545b81fb8c765334a5acc200ec24b4eecd
nari104 <172471497+nari104@users.noreply.github.com>
2024-08-23 21:22:41 +0000

    [update] bookers2 240824

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,21 @@ class BooksController < ApplicationController
   def new
     @books = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :caption)
+  end
+
 end
