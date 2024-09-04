d710fee533a65c24f9f608ed968f2cd381be00f8
nari104 <172471497+nari104@users.noreply.github.com>
2024-09-04 19:03:33 +0000

    [update] bookers2 240905

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,20 +20,36 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def show
     @books = Book.all
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to '/books'
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "updateに失敗しました"
+      @books = Book.all.order(id: :asc)
+      render :index
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :update, :edit, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
